9667a5249167c9185a41fb46dafb25055d8bbd99
Philip Munksgaard <pmunksgaard@gmail.com>
2014-10-26 15:52:57 +0100

    Add add_round_key
    Also includes a single unit test

--- src/lib.rs
+++ src/lib.rs
@@ -87,6 +87,14 @@ fn mix_columns(input: &[u8]) -> Vec<u8> {
     result
 }
 
+#[allow(dead_code)]
+fn add_round_key(input: &[u8], round_key: &[u8]) -> Vec<u8> {
+    assert_eq!(input.len(), 16);
+    assert_eq!(round_key.len(), 16);
+
+    Vec::from_fn(16, |idx| input[idx] ^ round_key[idx])
+}
+
 #[cfg(test)]
 mod test {
     use super::SBOX;
@@ -94,6 +102,7 @@ mod test {
     use super::shift_rows;
     use super::mix_column;
     use super::mix_columns;
+    use super::add_round_key;
 
     #[test]
     fn test_sbox() {
@@ -143,5 +152,23 @@ mod test {
                             0x81, 0x19, 0xD3, 0x26,
                             0xE5, 0x9A, 0x7A, 0x4C];
         assert_eq!(mix_columns(input.as_slice()), expected);
+    }
+
+    #[test]
+    fn test_add_round_key() {
+        let input = &[0x04, 0xE0, 0x48, 0x28,
+                      0x66, 0xCB, 0xF8, 0x06,
+                      0x81, 0x19, 0xD3, 0x26,
+                      0xE5, 0x9A, 0x7A, 0x4C];
+        let round_key = &[0xA0, 0x88, 0x23, 0x2A,
+                          0xFA, 0x54, 0xA3, 0x6C,
+                          0xFE, 0x2C, 0x39, 0x76,
+                          0x17, 0xB1, 0x39, 0x05];
+        let expected = vec![0xA4, 0x68, 0x6B, 0x02,
+                            0x9C, 0x9F, 0x5B, 0x6A,
+                            0x7F, 0x35, 0xEA, 0x50,
+                            0xF2, 0x2B, 0x43, 0x49];
+
+        assert_eq!(add_round_key(input, round_key), expected);
     }
 }
