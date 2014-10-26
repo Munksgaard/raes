70ca0b63312ba44d9068dcd2fe9f69eb370bdefd
Philip Munksgaard <pmunksgaard@gmail.com>
2014-10-27 00:09:47 +0100

    Remove unnecessary dead_code attributes

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![feature(slicing_syntax)]
 
 // Copied from Wikipedia: http://en.wikipedia.org/w/index.php?title=Rijndael_S-box&oldid=626170897
-#[allow(dead_code)]
 static SBOX: [u8, ..256] = [
      0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
      0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
@@ -26,13 +25,11 @@ static RCON: [[u8, ..4], ..10] = [
     [0x10, 0, 0, 0], [0x20, 0, 0, 0], [0x40, 0, 0, 0], [0x80, 0, 0, 0],
     [0x1B, 0, 0, 0], [0x36, 0, 0, 0]];
 
-#[allow(dead_code)]
 fn sub_bytes(input: &[u8]) -> Vec<u8> {
     let result: Vec<u8> = input.iter().map(|&x| SBOX[x as uint]).collect();
     return result;
 }
 
-#[allow(dead_code)]
 fn shift_rows(input: &[u8]) -> Vec<u8> {
     assert_eq!(input.len(), 16);
 
@@ -48,7 +45,6 @@ fn shift_rows(input: &[u8]) -> Vec<u8> {
 }
 
 // Adapted from Wikipedia: http://en.wikipedia.org/w/index.php?title=Rijndael_mix_columns&oldid=606147318#Implementation_example
-#[allow(dead_code)]
 fn mix_column(input: &[u8]) -> Vec<u8> {
     assert_eq!(input.len(), 4);
 
@@ -72,7 +68,6 @@ fn mix_column(input: &[u8]) -> Vec<u8> {
     result
 }
 
-#[allow(dead_code)]
 fn mix_columns(input: &[u8]) -> Vec<u8> {
     assert_eq!(input.len(), 16);
 
@@ -93,7 +88,6 @@ fn mix_columns(input: &[u8]) -> Vec<u8> {
     result
 }
 
-#[allow(dead_code)]
 fn add_round_key(input: &[u8], round_key: &[u8]) -> Vec<u8> {
     assert_eq!(input.len(), 16);
     assert_eq!(round_key.len(), 16);
@@ -101,7 +95,6 @@ fn add_round_key(input: &[u8], round_key: &[u8]) -> Vec<u8> {
     Vec::from_fn(16, |idx| input[idx] ^ round_key[idx])
 }
 
-#[allow(dead_code)]
 fn round_key(prev: &[u8], rcon: &[u8]) -> Vec<u8> {
     assert_eq!(prev.len(), 16);
     assert_eq!(rcon.len(), 4);
