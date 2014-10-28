463fe15e5b4b607e3f1f2aaec515767c3914c77e
Philip Munksgaard <pmunksgaard@gmail.com>
2014-10-28 16:02:29 +0100

    Name changes
     - encrypt_aes_ecb -> encode_aes_ecb
     - decrypt_aes_ecb -> decode_aes_ecb

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 mod aes;
 
 #[allow(dead_code)]
-pub fn encrypt_aes_ecb(plain: &[u8], key: &[u8]) -> Vec<u8> {
+pub fn encode_aes_ecb(plain: &[u8], key: &[u8]) -> Vec<u8> {
     assert_eq!(key.len(), 16);
     assert!(plain.len() % 16 == 0);
 
@@ -15,7 +15,7 @@ pub fn encrypt_aes_ecb(plain: &[u8], key: &[u8]) -> Vec<u8> {
 }
 
 #[allow(dead_code)]
-pub fn decrypt_aes_ecb(cipher: &[u8], key: &[u8]) -> Vec<u8> {
+pub fn decode_aes_ecb(cipher: &[u8], key: &[u8]) -> Vec<u8> {
     assert_eq!(key.len(), 16);
     assert!(cipher.len() % 16 == 0);
 
