c409c1175981511db9cf5215bef334156520bac4
Philip Munksgaard <pmunksgaard@gmail.com>
2014-10-28 13:05:09 +0100

    Add `encrypt_aes_ecb` and `decrypt_aes_ecb`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
 #![feature(slicing_syntax)]
 
 mod aes;
+
+#[allow(dead_code)]
+pub fn encrypt_aes_ecb(plain: &[u8], key: &[u8]) -> Vec<u8> {
+    assert_eq!(key.len(), 16);
+    assert!(plain.len() % 16 == 0);
+
+    let mut result: Vec<u8> = Vec::new();
+    for chunk in plain.chunks(16) {
+        result.push_all(aes::encrypt(chunk, key).as_slice());
+    }
+    result
+}
+
+#[allow(dead_code)]
+pub fn decrypt_aes_ecb(cipher: &[u8], key: &[u8]) -> Vec<u8> {
+    assert_eq!(key.len(), 16);
+    assert!(cipher.len() % 16 == 0);
+
+    let mut result: Vec<u8> = Vec::new();
+    for chunk in cipher.chunks(16) {
+        result.push_all(aes::decrypt(chunk, key).as_slice());
+    }
+    result
+}
